72b139db7ede481df5c4dbb177e776d224a4c46f
samirahamadjibo <samira.hamadjibo@gmail.com>
2023-07-05 16:02:54 +0200

    fix spring security context bug

--- src/main/java/com/digitalvitae/streetdico/controllers/UserController.java
+++ src/main/java/com/digitalvitae/streetdico/controllers/UserController.java
@@ -1,25 +1,11 @@
 package com.digitalvitae.streetdico.controllers;
 
 
-import com.digitalvitae.streetdico.entities.User;
-import com.digitalvitae.streetdico.entities.Word;
 import com.digitalvitae.streetdico.services.GoogleIdTokenVerifierService;
-import com.digitalvitae.streetdico.services.WordService;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 
 @RestController
 @Controller
--- src/main/java/com/digitalvitae/streetdico/controllers/WordController.java
+++ src/main/java/com/digitalvitae/streetdico/controllers/WordController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,24 +19,15 @@ public class WordController {
     @Autowired
     WordService wordService;
 
-    @GetMapping
-    public ResponseEntity<List<Word>> getWords(){
-        List<Word> words = new ArrayList<>();
-        words.add(new Word(1L, "Meuf", "Femme"));
-        words.add(new Word(1L, "Gechar", "Chargée"));
-        words.add(new Word(1L, "Mec", "Homme"));
-
-        return ResponseEntity.ok(words);
-    }
-    @DeleteMapping(path = "/word/{id}")
+    @DeleteMapping(path = "/{id}")
     public ResponseEntity<Long> deleteWord(@PathVariable("id") Long id){
         return ResponseEntity.ok(this.wordService.deleteWord(id));
     }
-    @GetMapping(path = "/all")
+    @GetMapping(path = "")
     public ResponseEntity<List<Word>> getTrendingWords(){
         return ResponseEntity.ok(this.wordService.getAllWords());
     }
-    @PostMapping(path = "/word", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "word", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Word> saveWord(@RequestBody Word word){
         return ResponseEntity.ok(this.wordService.saveWord(word));
     }
